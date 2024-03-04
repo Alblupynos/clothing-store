9710211e2672ad8a54900cf78c002e95b010be7c
Albina <alblupynos@gmail.com>
2024-03-04 15:00:24 +0200

    Setting Up Firebase.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,8 +13,8 @@ const Navigation = () => {
           <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to="/some">
-            Some link
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
